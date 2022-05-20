87955cac8159f8b3625edc98762c001e7674e65d
João Pedro <joaopetrussousa@gmail.com>
2022-05-20 19:02:39 -0300

    Requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = species.find((specie) => animal === specie.name)
+    .residents.every((idade) => idade.age >= age);
+  console.log(animals);
+  return animals;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  const getId = species.filter((specie) => ids.some((id) => id === specie.id));
+  return getId;
 }
 
 module.exports = getSpeciesByIds;
