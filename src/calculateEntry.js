30f5dff75fcb54d29b967dc045d5afb24bfe0ba3
João Pedro <joaopetrussousa@gmail.com>
2022-05-24 16:27:52 -0300

    Requisito 5

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,42 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  let kid = 0;
+  let adulto = 0;
+  let idoso = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) { kid += 1; }
+    if (element.age >= 18 && element
+      .age < 50) { adulto += 1; }
+    if (element.age >= 50) { idoso += 1; }
+  });
+  const total = { child: kid, adult: adulto, senior: idoso };
+  return total;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (Object.keys(entrants).length === 0) {
+    console.log('aqui');
+    return 0;
+  }
+  countEntrants(entrants);
+  let valor = 0;
+  /* return do countEntrants */.forEach((element) => {
+    valor = element.child * 20.99 + element.adult * 49.99 + element.senior * 24.99;
+  });
+  console.log(valor);
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+const entrants = [/* ]; */
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+calculateEntry(entrants);
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
+// eslint-disable-next-line max-lines-per-function
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const pegaAnimals = species.map((exemplar) => exemplar.name);
+    const pegaPopulation = species.map((exemplar) => exemplar.residents.length);
+    const allAnimals = {};
+    pegaAnimals.forEach((element, index) => {
+      allAnimals[element] = pegaPopulation[index];
+    });
+    return allAnimals;
+  }
+  if (Object.keys(animal).length === 1) {
+    const pegaAnimal = Object.values(animal);
+    const procuraAnimal = species.find(
+      (animalEspecifico) => animalEspecifico.name.includes(pegaAnimal),
+    );
+    return procuraAnimal.residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    console.log(animal.sex);
+    const pegaResidents = species.find((element) => element.name === animal.specie);
+    const procuraOutroAnimal = pegaResidents.residents.filter(
+      (animalPorEspecie) => animalPorEspecie.sex === animal.sex,
+    );
+    return procuraOutroAnimal.length;
+  }
 }
 
 module.exports = countAnimals;
