c8c534523d1d6c1637cf735d14d0609e3803e720
João Pedro <joaopetrussousa@gmail.com>
2022-05-24 18:45:55 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('handlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  test('handlerElephants retorna undefined se não ouver parametro de entrada', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  test('handlerElephants retorna uma string predeterminada se o parametro de entrada for diferente de string', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  test('handlerElephants retorna a quantidade de elefantes se o parametro de entrada for count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('handlerElephants retorna os nomes dos residentes de elefantes se o parametro de entrada for names', () => {
+    expect(handlerElephants('names')).toBe(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  /* test('getElephants retorna Defined', () => {
+    expect(typeof handlerElephants).toBe('function');
+  }); */
+});
