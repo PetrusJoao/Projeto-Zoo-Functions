455a9a4a62244a87c6f7094ec59adc89de459e43
João Pedro <joaopetrussousa@gmail.com>
2022-05-26 13:53:09 -0300

    Correção Lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,4 +1,4 @@
-const { employees, species } = require('../data/zoo_data');
+// const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
@@ -6,6 +6,3 @@ function getOldestFromFirstSpecies(id) {
 }
 
 module.exports = getOldestFromFirstSpecies;
-
-getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
-/* Nigel */
